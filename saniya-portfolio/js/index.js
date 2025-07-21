1c7e5f7bf66d469b0d5d067e6f5122be7ddd290e
sania <modysania@gmail.com>
2025-07-21 18:13:25 +0100

    Delete saniya-portfolio directory

--- saniya-portfolio/js/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-      const headers = document.querySelectorAll(".timeline-content-header");
-      headers.forEach(header => {
-        header.addEventListener("click", () => {
-          const desc = header.nextElementSibling;
-          desc.classList.toggle("open");
-        });
-      });
-    });
